ac12b4d8c3888a0c3d1f80c585d4fedf70c4b6f9
Pedro R. Andrade <pedro.andrade@inpe.br>
2025-02-20 09:00:02 -0300

    Improving visualisation part

--- main.R
+++ main.R
@@ -28,9 +28,11 @@ data <- terra::readRDS("amz_fc/tab/prodes/tab4id.rds") |>
     new_fm > 15 ~ "large"
   ))
 
+dim(data) # 470.645 rural properties
+
 sf::read_sf("amz_fc/shape/amz_land_tenure.shp") |>
   dplyr::inner_join(data, by = "id") |>
-  sf::write_sf("result/car-legal-reserve.shp")
+  sf::write_sf("result/car-legal-reserve.gpkg")
 
 ###########################################################################
 # Plotting
@@ -39,90 +41,47 @@ sf::read_sf("amz_fc/shape/amz_land_tenure.shp") |>
 # Figure XX of the paper, histogram of legal reserve percentage
 hist(data$lr_porc)
 
-dim(data) # 470.645 rural properties
-
-antes = table(data$tamanho)
-depois = table(data$n_tamanho)
-
 ticks <- c(0.1, 1, 10, 100)
 
-log_valores1 = log10(data$area_fm)
-log_valores2 = log10(result$new_fm)
+log_v1 = log10(data$area_fm)
+log_v2 = log10(result$new_fm)
 
-breaks <- seq(floor(min(c(log_valores1, log_valores2), na.rm=T)), 
-              ceiling(max(c(log_valores1, log_valores2), na.rm=T)), by = 0.25)
+breaks <- seq(floor(min(c(log_v1, log_v2), na.rm = T)), 
+              ceiling(max(c(log_v1, log_v2), na.rm = T)), by = 0.25)
 
 # Figure XX of the paper, histogram of area of rural properties in fiscal modules
-hist1 <- hist(log_valores1, breaks = breaks, col="blue", xaxt = "n")
+hist1 <- hist(log_v1, breaks = breaks, col="blue", xaxt = "n")
 axis(1, at = log10(ticks), labels = round(ticks, 1))
 
 # Figure XX plotting two histograms of areas together
-hist1 <- hist(log_valores1, breaks = breaks, col="blue", xaxt = "n")
-hist2 <- hist(log_valores2, breaks = breaks, col=rgb(1, 0, 0, 0.5), add=T, xaxt = "n" )
+hist1 <- hist(log_v1, breaks = breaks, col="blue", xaxt = "n")
+hist2 <- hist(log_v2, breaks = breaks, col=rgb(1, 0, 0, 0.5), add=T, xaxt = "n" )
 axis(1, at = log10(ticks), labels = round(ticks, 1))
 
-# Combine frequencies into a matrix
-counts <- rbind(hist1$counts, hist2$counts)
+# Table with the changes in the size of the properties
 
-# Create side-by-side barplot
-barplot(counts, beside = TRUE, col = c("red", "blue"),
-        legend = c("Dataset 1", "Dataset 2"),
-        main = "Side-by-Side Histogram", xlab = "Value", ylab = "Frequency")
-ticks <- c(0.1, 1, 10, 100, 1000)
-axis(1, at = log10(ticks), labels = round(ticks, 1))
+before <- table(data$old_class)
+after <- table(data$new_class)
 
-
-
-# tabela com as mudancas dos tamanhos das propriedades
-mytable = rbind(antes, depois, delta = depois - antes, diff_perc = round(depois / antes * 100) - 100)
+mytable <- rbind(before, after, delta = after - before, diff_perc = round(after / before * 100) - 100)
+mytable <- mytable[, 3:1]
 
 kableExtra::kable(mytable, format = "latex")
 
-par(mfcol = c(1, 2))
-hist(log(data$area_fm), ylim = c(0, 140000), main = "Antes")
-hist(log(result$new_fm), ylim = c(0, 140000), main = "Depois")
-
-data
-
-r2 <- result |>
-  dplyr::filter(area_fm < 75)
-hist(r2$area_fm)
-
-r2 <- result |>
-  dplyr::filter(area_fm < 10)
-hist(r2$area_fm)
-
-
-change_to_less_than_4 <- result |> 
-  dplyr::filter(new_fm < 4) |>
-  dplyr::filter(area_fm > 4) 
-
-# 26.179 propriedades tinham mais de 4MF e passaram a ter menos
-# ou 5.57% de todas as propriedades
-dim(change_to_less_than_4)
-nrow(change_to_less_than_4) / nrow(result) * 100
-
-change_to_less_than_4
-
-names(change_to_less_than_4)
-
-change_to_less_than_4$inside = 1
-
-sum(change_to_less_than_4$lr_balance)
-
-
-amz <- sf::read_sf("amz_land_tenure.shp")
-amz$id = as.integer(amz$id)
 
 uf_map <- tibble(
   uf_code = paste0(c(12, 13, 16, 15, 21, 51, 11, 14, 17)),
   sigla_uf = c("AC", "AM", "AP", "PA", "MA", "MT", "RO", "RR", "TO")
 )
 
+df <- sf::read_sf("result/car-legal-reserve.gpkg") |>
+  sf::st_drop_geometry(shp) |>
+  dplyr::left_join(uf_map, by = "uf_code")
 
-fresult <- amz %>%
-  inner_join(change_to_less_than_4, by = "id") |>
-  left_join(uf_map, by = "uf_code")
+result <- df |> 
+  dplyr::group_by(sigla_uf, old_class, new_class) |>
+  dplyr::summarise(n = n(), .groups = "drop") |>
+  dplyr::filter(!is.na(new_class)) 
 
-table(fresult$sigla_uf)
 
+View(result)
